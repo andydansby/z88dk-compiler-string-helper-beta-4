337f06b0c62bbbcf9fa6cc02a9252faedea81219
Andrew Dansby <andydansby@gmail.com>
2016-10-22 05:26:55 -0400

    Add files via upload

---
+++ z88dk-compile-options-helper-beta/AboutBox1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z88dk_compile_options_helper_beta
+{
+	partial class AboutBox1 : Form
+	{
+		public AboutBox1()
+		{
+			InitializeComponent();
+			this.Text = String.Format("About {0}", AssemblyTitle);
+			//this.labelProductName.Text = AssemblyProduct;
+			//this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+			//this.labelCopyright.Text = AssemblyCopyright;
+			//this.labelCompanyName.Text = AssemblyCompany;
+			//this.textBoxDescription.Text = AssemblyDescription;
+			//textBoxDescription.Text = "Z88dk Helper";
+
+			this.labelProductName.Text = "Z88dk Helper";
+			this.labelVersion.Text = ".5 Beta";
+			this.labelCopyright.Text = "2016 Open Source / Freeware";
+			this.labelCompanyName.Text = "Andy Dansby";
+			//.2 Alpha
+
+		}
+
+		#region Assembly Attribute Accessors
+
+		public string AssemblyTitle
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+				if (attributes.Length > 0)
+				{
+					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+					if (titleAttribute.Title != "")
+					{
+						return titleAttribute.Title;
+					}
+				}
+				return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+			}
+		}
+
+		public string AssemblyVersion
+		{
+			get
+			{
+				return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			}
+		}
+
+		public string AssemblyDescription
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+		}
+
+		public string AssemblyProduct
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "Z88dk Helper";
+				}
+				return ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+		}
+
+		public string AssemblyCopyright
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "Open Source / Freeware";
+				}
+				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+		}
+
+		public string AssemblyCompany
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+		}
+		#endregion
+
+		private void okButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
